59803ddedb8b55e89e1884033c2747d9cdc11237
Vipul <vipuldamrepro26@gmail.com>
2024-07-12 18:31:26 +0530

    added social link to icons

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -6,13 +6,13 @@ const SocialMedia = () => {
   return (
     <div className='app__social'>
     <div>
-        <BsLinkedin className='social-icon' />
+        <a  className='social-icon' href="https://www.linkedin.com/in/vipul-damre-2237241a7/" target='_blank'><BsLinkedin /></a>
     </div>
     <div>
-        <BsGithub className='social-icon' />
+        <a className='social-icon' href="https://github.com/Vipuldamre26" target='_blank'><BsGithub /></a>
     </div>
     <div>
-        <BsInstagram className='social-icon' />
+        <a className='social-icon' href="https://www.instagram.com/vipulllll_k_d_?igsh=bnQxNGxmMHVsZ2lq&utm_source=qr" target='_blank'><BsInstagram /></a>
     </div>
     </div>
   )
